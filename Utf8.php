739b6f600855067a2110fe8aecc909136c53029e
vensko <github.com@belbit.net>
2015-09-30 02:37:00 +0200

    Optimization

--- Utf8.php
+++ Utf8.php
@@ -32,30 +32,32 @@ class Utf8
 	public static $invalid1251Seq = [];
 
 	public static $nonCyrillic1251 = [
-		"\x8E", // Z with caron
-		"\x9E", // z with caron
-		"\x8F", // Z with acute accent
-		"\x9F", // z with acute accent
-		"\x8C", // non-cyrillic
-		"\x9C", // non-cyrillic
-		"\x8A", // non-cyrillic
-		"\x9A", // non-cyrillic
+		142 => "\x8E", // Z with caron
+		158 => "\x9E", // z with caron
+		143 => "\x8F", // Z with acute accent
+		159 => "\x9F", // z with acute accent
+		140 => "\x8C", // non-cyrillic
+		156 => "\x9C", // non-cyrillic
+		138 => "\x8A", // non-cyrillic
+		154 => "\x9A", // non-cyrillic
 	];
 
 	public static $diacritics1250 = [
-		"\xA3", // L with stroke
-		"\xB3", // l with stroke
-		"\x8F", // Z with acute accent
-		"\x9F", // z with acute accent
-		"\xAF", // Z with dot
-		"\xBF", // z with dot
-		"\xA5", // A with ogonek
-		"\xB9", // a with ogonek
-		"\xAA", // S with ogonek
-		"\xBA", // s with ogonek
-		"\xBC", // L with acute accent
-		"\xBE", // l with acute accent
+		163 => "\xA3", // L with stroke
+		179 => "\xB3", // l with stroke
+		143 => "\x8F", // Z with acute accent
+		159 => "\x9F", // z with acute accent
+		175 => "\xAF", // Z with dot
+		191 => "\xBF", // z with dot
+		165 => "\xA5", // A with ogonek
+		185 => "\xB9", // a with ogonek
+		170 => "\xAA", // S with ogonek
+		186 => "\xBA", // s with ogonek
+		188 => "\xBC", // L with acute accent
+		190 => "\xBE", // l with acute accent
 	];
+
+	protected static $charCount = [];
 
 	/**
 	 * Ensures that a string is UTF-8 encoded
@@ -102,31 +104,17 @@ class Utf8
 	 */
 	public static function detectEncoding($str)
 	{
-		$enc = static::detectASCII($str)
-			OR $enc = static::detectBOM($str)
-			OR $enc = static::detectUtf16WithoutBOM($str)
-			OR $enc = static::detectUTF8($str)
-			OR $enc = static::detectSingleByteEncoding($str);
-
-		return $enc;
-	}
-
-	/**
-	 * Detects ISO-8859-1 and Windows 1250, 1251
-	 *
-	 * @param string $str
-	 * @return string
-	 */
-	public static function detectSingleByteEncoding($str)
-	{
-		$enc = static::detectWinByUniqueCode($str)
-			OR $enc = static::detectWin1251($str)
-			OR $enc = static::detectWin1250($str)
-			OR $enc = static::detectISO88591($str);
-
-		if (!$enc) {
-			$enc = static::mayBeWin1251($str) ? static::CP1251 : static::ISO88591;
+		if ($str === '') {
+			return static::ASCII;
 		}
+
+		static::$charCount = [];
+
+		$enc = static::detectBOM($str)
+			OR $enc = static::detectUtf16WithoutBOM($str)
+			OR $enc = static::detectASCII($str)
+			OR $enc = static::detectUTF8($str)
+			OR $enc = static::detectSingleByteEncoding($str, static::$charCount);
 
 		return $enc;
 	}
@@ -135,45 +123,172 @@ class Utf8
 	 * @param string $str
 	 * @return null|string
 	 */
-	public static function detectWin1250($str)
+	public static function detectASCII($str)
+	{
+		if ($str === '') {
+			return static::ASCII;
+		}
+
+		// count_chars returns the characters in ascending octet order
+		static::$charCount = count_chars($str, 1);
+
+		// Check for null character
+		if (!key(static::$charCount)) {
+			return null;
+		}
+
+		end(static::$charCount);
+
+		// Check for 8-bit character
+		if (key(static::$charCount) > 127) {
+			return null;
+		}
+
+		return static::ASCII;
+	}
+
+	/**
+	 * @param string $str
+	 * @return null|string
+	 */
+	public static function detectUtf16WithoutBOM($str)
+	{
+		$nullPos = strpos($str, "\x00");
+
+		if ($nullPos !== false) {
+			if (strpos($str, "\x2000") !== false) {
+				return static::UTF16LE;
+			} else if (strpos($str, "\x0020") !== false) {
+				return static::UTF16BE;
+			} else {
+				return $nullPos % 2 === 0 ? static::UTF16BE : static::UTF16LE;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * @param string $str
+	 * @return null|string
+	 */
+	public static function detectUTF8($str)
+	{
+		return mb_check_encoding($str, 'UTF-8') ? static::UTF8 : null;
+	}
+
+	/**
+	 * @param string $str
+	 * @return null|string
+	 */
+	public static function detectBOM($str)
+	{
+		if (isset($str[1])) {
+			$bom = $str[0].$str[1];
+
+			if (static::UTF16BE_BOM === $bom) {
+				return static::UTF16BE;
+			}
+
+			if (static::UTF16LE_BOM === $bom) {
+				if (isset($str[3]) && static::UTF32LE_BOM === $bom.$str[2].$str[3]) {
+					return 'UTF-32LE';
+				} else {
+					return static::UTF16LE;
+				}
+			}
+
+			if (isset($str[2])) {
+				$bom .= $str[2];
+
+				if (static::UTF8_BOM === $bom) {
+					return static::UTF8;
+				}
+
+				if (static::UTF7_BOM === $bom) {
+					return 'UTF-7';
+				}
+
+				if ("\x00\x00" === $bom && isset($str[3]) && static::UTF32BE_BOM === $bom.$str[2].$str[3]) {
+					return 'UTF-32BE';
+				}
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Detects ISO-8859-1 and Windows 1250, 1251
+	 *
+	 * @param string $str
+	 * @param array $chars
+	 * @return string
+	 */
+	public static function detectSingleByteEncoding($str, $chars = [])
+	{
+		$enc = static::detectWinByUniqueCode($chars)
+		OR $enc = static::detectWin1251($chars, $str)
+		OR $enc = static::detectWin1250($chars, $str)
+		OR $enc = static::detectISO88591($str);
+
+		if (!$enc) {
+			$enc = static::mayBeWin1251($chars, $str) ? static::CP1251 : static::ISO88591;
+		}
+
+		return $enc;
+	}
+
+	/**
+	 * @param array $chars
+	 * @param string $str
+	 * @return null|string
+	 */
+	public static function detectWin1250(array $chars, $str)
 	{
 		// euro
-		if (strpos($str, "\x88") !== false) {
+		if (isset($chars[136])) {
 			return null;
 		}
 
 		// a with ogonek
-		if (($pos = strpos($str, "\xB9")) !== false && $pos !== 0) {
-			if (!isset($str[$pos + 1])) {
-				return static::CP1250;
-			}
+		if (isset($chars[185])) {
+			$pos = strpos($str, "\xB9");
 
-			$prev = ord($str[$pos - 1]);
+			if ($pos !== 0) {
+				if (!isset($str[$pos + 1])) {
+					return static::CP1250;
+				}
 
-			if ($prev > 64 && $prev < 123) {
-				return static::CP1250;
-			}
+				$prev = ord($str[$pos - 1]);
 
-			$next = ord($str[$pos + 1]);
+				if ($prev > 64 && $prev < 123) {
+					return static::CP1250;
+				}
 
-			if (($next > 96 && $next < 123) || $next === 179) {
-				return static::CP1250;
+				$next = ord($str[$pos + 1]);
+
+				if (($next > 96 && $next < 123) || $next === 179) {
+					return static::CP1250;
+				}
 			}
 		}
 
-		foreach (static::$diacritics1250 as $i) {
-			if (($pos = strpos($str, $i)) !== false) {
-				if (isset($str[$pos + 1])) {
-					$next = ord($str[$pos + 1]);
-					// next or previous is ASCII letter
-					if (($next > 64 && $next < 123) || $next === 163) {
+		foreach (static::$diacritics1250 as $index => $i) {
+			if (isset($chars[$index])) {
+				$pos = strpos($str, $i);
+
+				// next or previous is ASCII letter
+				if ($pos > 0) {
+					$prev = ord($str[$pos - 1]);
+					if (($prev > 64 && $prev < 123) || $prev === 163) {
 						return static::CP1250;
 					}
-					if ($pos > 0) {
-						$prev = ord($str[$pos - 1]);
-						if (($prev > 64 && $prev < 123) || $prev === 163) {
-							return static::CP1250;
-						}
+				}
+				if (isset($str[$pos + 1])) {
+					$next = ord($str[$pos + 1]);
+					if (($next > 64 && $next < 123) || $next === 163) {
+						return static::CP1250;
 					}
 				}
 			}
@@ -196,58 +311,58 @@ class Utf8
 	}
 
 	/**
-	 * @param string $str
+	 * @param array $chars
 	 * @return null|string
 	 */
-	public static function detectWinByUniqueCode($str)
+	public static function detectWinByUniqueCode(array $chars)
 	{
 		// Cyrillic small ya
-		if (strpos($str, "\xFF") !== false) {
+		if (isset($chars[255])) {
 			return static::CP1251;
 		}
 
 		// Cyrillic yo
-		if (strpos($str, "\xB8") !== false) {
+		if (isset($chars[184])) {
 			return static::CP1251;
 		}
 
 		// Cyryllic Belarusian I
-		if (strpos($str, "\xB2") !== false) {
+		if (isset($chars[178])) {
 			return static::CP1251;
 		}
 
 		// Cyrillic u short
-		if (strpos($str, "\xA2") !== false) {
+		if (isset($chars[162])) {
 			return static::CP1251;
 		}
 
 		// Euro
-		if (strpos($str, "\x88") !== false) {
+		if (isset($chars[136])) {
 			return static::CP1251;
 		}
 
 		// L with stroke
-		if (strpos($str, "\xA3") !== false) {
+		if (isset($chars[163])) {
 			return static::CP1250;
 		}
 
 		// T with caron
-		if (strpos($str, "\x8D") !== false) {
+		if (isset($chars[141])) {
 			return static::CP1250;
 		}
 
 		// Z with acute accent
-		if (strpos($str, "\x8F") !== false) {
+		if (isset($chars[143])) {
 			return static::CP1250;
 		}
 
 		// z with acute accent
-		if (strpos($str, "\x9F") !== false) {
+		if (isset($chars[159])) {
 			return static::CP1250;
 		}
 
 		// Arabic
-		if (strpos($str, "\x90") !== false) {
+		if (isset($chars[144])) {
 			return static::CP1256;
 		}
 
@@ -255,18 +370,24 @@ class Utf8
 	}
 
 	/**
+	 * @param array $chars
 	 * @param string $str
 	 * @return null|string
 	 */
-	public static function detectWin1251($str)
+	public static function detectWin1251(array $chars, $str)
 	{
 		// Cyrillic capital and small ch
-		if ((($pos = strpos($str, "\xF7")) !== false || ($pos = strpos($str, "\xD7")) !== false) && ($pos === 0 || !ctype_digit($str[$pos - 1]))) {
-			return static::CP1251; // also 1253, 1255
+		if (isset($chars[247]) || isset($chars[215])) {
+			$pos = strpos($str, isset($chars[247]) ? "\xF7" : "\xD7");
+			if ($pos === 0 || !ctype_digit($str[$pos - 1])) {
+				return static::CP1251; // also 1253, 1255
+			}
 		}
 
 		// Either Cyrillic capital YA or German eszett
-		if (($pos = strpos($str, "\xDF")) !== false) {
+		if (isset($chars[223])) {
+			$pos = strpos($str, "\xDF");
+
 			if ($pos === 0) {
 				return static::CP1251;
 			}
@@ -284,7 +405,9 @@ class Utf8
 		}
 
 		// number sign or a with ogonek
-		if (($pos = strpos($str, "\xB9")) !== false) {
+		if (isset($chars[185])) {
+			$pos = strpos($str, "\xB9");
+
 			if ($pos === 0) {
 				return static::CP1251;
 			}
@@ -304,15 +427,14 @@ class Utf8
 	}
 
 	/**
+	 * @param array $chars
 	 * @param string $str
 	 * @return bool
 	 */
-	public static function mayBeWin1251($str)
+	public static function mayBeWin1251(array $chars, $str)
 	{
-		foreach (static::$nonCyrillic1251 as $i) {
-			if (strpos($str, $i) !== false) {
-				return false;
-			}
+		if (array_intersect_key($chars, static::$nonCyrillic1251)) {
+			return false;
 		}
 
 		$i = 0;
@@ -369,89 +491,11 @@ class Utf8
 		$c = mb_convert_encoding($c, 'Windows-1251', 'UTF-8');
 		$c = explode("\n", $c);
 
+		static::$invalid1251Seq = [];
+
 		foreach ($c as $line) {
 			list($first, $second) = explode(':', $line);
 			static::$invalid1251Seq[$first] = array_fill_keys(explode(',', $second), true);
 		}
-	}
-
-	/**
-	 * @param string $str
-	 * @return null|string
-	 */
-	public static function detectUtf16WithoutBOM($str)
-	{
-		$nullPos = strpos($str, "\x00");
-
-		if ($nullPos !== false) {
-			if (strpos($str, "\x2000") !== false) {
-				return static::UTF16LE;
-			} else if (strpos($str, "\x0020") !== false) {
-				return static::UTF16BE;
-			} else {
-				return $nullPos % 2 === 0 ? static::UTF16BE : static::UTF16LE;
-			}
-		}
-
-		return null;
-	}
-
-	/**
-	 * @param string $str
-	 * @return null|string
-	 */
-	public static function detectASCII($str)
-	{
-		return preg_match('/^[\x09\x0A\x0D\x20-\x7E]+$/', $str) ? static::ASCII : null;
-	}
-
-	/**
-	 * @param string $str
-	 * @return null|string
-	 */
-	public static function detectUTF8($str)
-	{
-		return preg_match('//u', $str) ? static::UTF8 : null;
-	}
-
-	/**
-	 * @param string $str
-	 * @return null|string
-	 */
-	public static function detectBOM($str)
-	{
-		if (isset($str[1])) {
-			$bom = $str[0].$str[1];
-
-			if (static::UTF16BE_BOM === $bom) {
-				return static::UTF16BE;
-			}
-
-			if (static::UTF16LE_BOM === $bom) {
-				if (isset($str[3]) && static::UTF32LE_BOM === $bom.$str[2].$str[3]) {
-					return 'UTF-32LE';
-				} else {
-					return static::UTF16LE;
-				}
-			}
-
-			if (isset($str[2])) {
-				$bom .= $str[2];
-
-				if (static::UTF8_BOM === $bom) {
-					return static::UTF8;
-				}
-
-				if (static::UTF7_BOM === $bom) {
-					return 'UTF-7';
-				}
-
-				if ("\x00\x00" === $bom && isset($str[3]) && static::UTF32BE_BOM === $bom.$str[2].$str[3]) {
-					return 'UTF-32BE';
-				}
-			}
-		}
-
-		return null;
 	}
 }
